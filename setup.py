d520ee5fdd5b02b319b074b7ae1b958f6610b8e3
free-x <oroitburd@gmail.com>
2021-01-23 14:20:08 +0100

    Correct wxPython

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup (
 	author="Sailoog",
 	author_email='info@sailoog.com',
 	url='https://github.com/openplotter/openplotter-settings',
-        install_requires=['wxgtk4.0','pyudev','ujson'],
+        install_requires=['wxPython','pyudev','ujson'],
 	packages=['openplotterSettings'],
 	classifiers = ['Natural Language :: English',
 	'Operating System :: POSIX :: Linux',
